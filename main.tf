1e6275e05188f0d88530eb29c5f5ada95ba7890d
tovanhieu <tovanhieu1996.hl@gmail.com>
2023-04-14 16:23:11 +0700

    update instance type 2

--- main.tf
+++ main.tf
@@ -23,5 +23,9 @@ data "aws_ami" "ami" {
 
 resource "aws_instance" "ansible_server" {
   ami           = data.aws_ami.ami.id
-  instance_type = "t3.small" // t3.micro -> t3.small
+  instance_type = "t3.small"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
