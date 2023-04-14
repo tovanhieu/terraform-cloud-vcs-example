e2b778c389417fc839cb91325a1355d9bf7b0cc5
tovanhieu <tovanhieu1996.hl@gmail.com>
2023-04-14 15:48:51 +0700

    init code

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "ansible_server" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+}
